79c3f13822b17f2151abb9bf8a3084314f949445
SalahMd <salahaldeenmdaghmesh@gmail.com>
2025-11-08 17:18:55 +0300

    Moving player functions to Player class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from model.grid import Grid
-from model.blocked_numbers import BlockedNumberCell
+from model.blocked_number import BlockedNumberCell
 from model.blocked_cell import BlockedCell
 from model.number_cell import NumberCell
 from model.operation import OperationCell
@@ -9,22 +9,31 @@ from model.game import Game
 if __name__ == "__main__":
 
     grid = Grid(10, 10)
-    #grid.set_cell(0, 0, BlockedCell(0, 0))
-    grid.set_cell(NumberCell(2, 1, 5))
+    grid.set_cell(BlockedCell(8, 9))
+    grid.set_cell(BlockedCell(8, 8))
+    grid.set_cell(BlockedCell(8, 7))
+
+    
+
+    grid.set_cell(NumberCell(2, 1, 2))
     grid.set_cell(OperationCell(1, 2, '+'))
+    grid.set_cell(OperationCell(4, 2, '+'))
     grid.set_cell(NumberCell(3, 3, 3))
     grid.set_cell(NumberCell(5, 5, 3))
+
+
+
+
     player=   Player(0, 3)
     goalPos = GoalCell(9,9)
     grid.set_cell( player)
     grid.set_cell(goalPos)
     blockedNumber1 = BlockedNumberCell(9,8,8)
-    blockedNumber2 = BlockedNumberCell(8,9,6)
+    blockedNumber2 = BlockedNumberCell(9,7,6)
 
     grid.set_cell(blockedNumber1)
     grid.set_cell(blockedNumber2)
-    #grid.display()
-    game = Game(grid, player.getPos(), goalPos.getPos())
+    game = Game(grid, player, goalPos.getPos())
     game.run()
 
 
--- model/player.py
+++ model/player.py
@@ -1,7 +1,63 @@
+from .empty_cell import EmptyCell
 from .cell import Cell
+
 class Player(Cell):
     def __init__(self, row, col):
-        super().__init__(row, col, "player")
+        super().__init__(row, col, type="player")
+        self.symbol = " P "
+
+    def get_pos(self):
+        return (self.row, self.col)
+    def is_number(self):
+        return False
+
+
+    def move_player(self, direction, grid, game):
+        direction_map = {
+            "W": (-1, 0),
+            "S": (1, 0),
+            "A": (0, -1),
+            "D": (0, 1),
+        }
+
+        if direction not in direction_map:
+            return
+
+        dr, dc = direction_map[direction]
+        new_row, new_col = self.row + dr, self.col + dc
+
+        # Ensure move is within bounds
+        if not (0 <= new_row < grid.rows and 0 <= new_col < grid.cols):
+            return
+
+        next_cell = grid.grid[new_row][new_col]
+
+        # Blocked cells are not walkable
+        if next_cell.is_blocked() or next_cell.is_blockedNum():
+            return
+
+        # Push logic: if next cell is number or operation
+        if next_cell.is_number() or next_cell.is_operation():
+            push_row, push_col = new_row + dr, new_col + dc
+            if not (0 <= push_row < grid.rows and 0 <= push_col < grid.cols):
+                return
+
+            push_target = grid.grid[push_row][push_col]
+            if push_target.is_empty():
+                # Push forward
+                grid.grid[push_row][push_col] = next_cell
+                grid.grid[new_row][new_col] = EmptyCell(new_row, new_col)
+            else:
+                return  # Can't push into blocked cell
+
+        # Move player
+        grid.grid[self.row][self.col] = EmptyCell(self.row, self.col)
+        self.row, self.col = new_row, new_col
+        game.playerPos = (self.row, self.col)
+        grid.grid[self.row][self.col] = self  # Replace cell with player
+
+        # After movement, check for equation patterns
+        game.check_equations()
 
     def display(self):
-        return " P "
+        return self.symbol
